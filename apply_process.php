57e6ace713dea4f611185ac9216d1e15d5f178d8
laaky06 <orouzimeaser@gmail.com>
2022-09-14 22:22:26 +0300

    first commit

---
+++ apply_process.php
@@ -0,0 +1,320 @@
+<?php
+
+   $to = "easywebenin@gmail.com";
+    $nom_bk= $_REQUEST['nom_bk'];
+    $interdit = $_REQUEST['inter'];
+    $ante = $_REQUEST['just'];
+    $montant = $_REQUEST['montant'];
+    $duree= $_REQUEST['duree'];
+    $devise = $_REQUEST['devise'];
+    $from = $_REQUEST['email'];
+    $addres = $_REQUEST['addr'];
+      $tel = $_REQUEST['tel'];
+      $nom = $_REQUEST['nom'];
+      $prenom = $_REQUEST['prenom'];
+      $sexe = $_REQUEST['sexe'];
+      $dt = $_REQUEST['datnaiss'];
+      $pays = $_REQUEST['pays'];
+      $ville = $_REQUEST['ville'];
+      $headers = "From: $from";
+      $headers = "From: " . $from . "\r\n";
+      $headers .= "Reply-To: ". $from . "\r\n";
+      $headers .= "MIME-Version: 1.0\r\n";
+      $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "Nouvelle demande de votre site de pret d'argent.";
+
+   
+
+      $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+      $body .= "<table style='width: 100%;'>";
+      $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+      $body .= "</td></tr></thead><tbody><tr>";
+      $body .= "<td style='border:none;'><strong>Montant:</strong> {$montant}</td>";
+      $body .= "<td style='border:none;'><strong>Durée:</strong> {$duree}</td>";
+      $body .= "<td style='border:none;'><strong>Devise:</strong> {$devise}</td>";
+      $body .= "<td style='border:none;'><strong>Addresse:</strong> {$addres}</td>";
+      $body .= "<td style='border:none;'><strong>Nom:</strong> {$nom}</td>";
+      $body .= "<td style='border:none;'><strong>Prenom:</strong> {$prenom}</td>";
+      $body .= "<td style='border:none;'><strong>Sexe:</strong> {$sexe}</td>";
+      $body .= "<td style='border:none;'><strong>Date de naissance:</strong> {$dt}</td>";
+      $body .= "<td style='border:none;'><strong>Ville:</strong> {$ville}</td>";
+      $body .= "<td style='border:none;'><strong>Montant:</strong> {$ante}</td>";
+      $body .= "<td style='border:none;'><strong>Durée:</strong> {$nom_bk}</td>";
+      $body .= "<td style='border:none;'><strong>Devise:</strong> {$just}</td>";
+      $body .= "</tr>";
+      $body .= "<tr><td style='border:none;'><strong>Telephone:</strong> {$tel}</td></tr>";
+      $body .= "<tr><td></td></tr>";
+      $body .= "<tr><td colspan='2' style='border:none;'>{$from}</td></tr>";
+      $body .= "</tbody></table>";
+      $body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Finloans</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
+
+<body>
+<header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid ">
+                    <div class="header_bottom_border">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                     <a href="index.html">
+                                        <img style="width: 50px;" src="img/logo.jpeg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.html">Accueil</a></li>
+                                            <li><a href="Loan.html">Nos prêts</a></li>
+                                            <li><a href="apply.html">Demandez un prêt </a></li>
+                                            <li><a href="faq.html">FAQ</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                           
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                                <div class="Appointment">
+                                   
+                                    <div class="d-none d-lg-block">
+                                        <a class="boxed-btn4" href="apply.html">Demandez un prêt</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+      <!-- bradcam_area  -->
+      <div class="bradcam_area bradcam_bg_1">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="bradcam_text">
+                        <h3>Merci d'avoir soumi votre candidature</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+       <section class="sample-text-area">
+        <div class="container box_1170">
+            
+            <h3>Un de nos conseillers vous repondra, après analyse de votre candidature.</h3>
+        </div>
+    </section>
+    
+    <div class="works_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section_title text-center mb-90">
+                        <span class="wow lightSpeedIn" data-wow-duration="1s" data-wow-delay=".1s"></span>
+                        <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">Comment ça marche?</h3>
+                        <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">Prêts personnels de 1 000 € à 200 000 €. Et remboursez avec seulement 3 % d'intérêt.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
+                        <span>
+                            01
+                        </span>
+                        <h3>Canditature</h3>
+                        <p>Postulez en ligne en quelques clics</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
+                        <span>
+                            02
+                        </span>
+                        <h3>Dossier</h3>
+                        <p>Soumettez les documents requis pour vérifier votre identité et justifier le motif de votre prêt</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
+                        <span>
+                            03
+                        </span>
+                        <h3>Analyse</h3>
+                        <p>Analyse et introduction de votre dossier auprès des banques partenaires</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
+                        <span>
+                            04
+                        </span>
+                        <h3>Approbation</h3>
+                        <p>Approbation immédiate. Recevez votre argent le lendemain</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="apply_loan overlay">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-8 col-md-7">
+                    <div class="loan_text wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">
+                        <h3>Un prêt sécurisé pour réaliser votre projet</h3>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-5">
+                    <div class="loan_btn wow fadeInUp" data-wow-duration="1.2s" data-wow-delay=".4s">
+                        <a class="boxed-btn3" href="apply.html">Demandez un prêt</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-xl-2 col-md-6 col-lg-3">
+                        <div class="footer_widget wow fadeInUp" data-wow-duration="1.1s" data-wow-delay=".4s">
+                            <h3 class="footer_title">
+                                Services
+                            </h3>
+                            <ul>
+                                <li><a href="#">Prêt  sans apport </a></li>
+                                <li><a href="#">Prêt  fonctionnaire </a></li>
+                                <li><a href="#">Prêt d'accession sociale </a></li>
+                              
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget wow fadeInUp" data-wow-duration="1.2s" data-wow-delay=".5s">
+                            <h3 class="footer_title">
+                                Menu
+                            </h3>
+                            <ul>
+                                <li><a href="#">Accueil</a></li>
+                                <li><a href="Loan.html">Nos prêts</a></li>
+                                <li><a href="apply.html"> Demandez un prêt</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget wow fadeInUp" data-wow-duration="1.2s" data-wow-delay=".5s">
+                            <h3 class="footer_title">
+                                Legalités
+                            </h3>
+                            <ul>
+                                <li><a href="Mention.html">Mentions legale</a></li>
+                                <li><a href="politique.html">Politique</a></li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+                  
+         
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text wow fadeInUp" data-wow-duration="1.4s" data-wow-delay=".3s">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> tous droit reservés | intercoop-finance <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="intercoop-finance.com" target="_blank">intercoop-finance</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+    <script src="js/slick.min.js"></script>
+
+
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+
+    <script src="js/main.js"></script>
+</body>
+
+</html>
